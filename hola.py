c0e83630041b5e3253161d50b49830efb0faaee7
User <ug-fca-gig@ug.edu.ec>
2025-04-23 16:34:04 -0500

    se creo la palabra person

--- hola.py
+++ hola.py
@@ -1,1 +1,9 @@
-print("hola mundo")
+class Persona:
+    def __init__(self, nombre, apellido):
+        self.nombre = nombre
+        self.apellido = apellido
+
+if __name__ == '__main__':
+    oPersona = Persona("Juan", "Perez")
+    print(oPersona.nombre)
+    print(oPersona.apellido)
